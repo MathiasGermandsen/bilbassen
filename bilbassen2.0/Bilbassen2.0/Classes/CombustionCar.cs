1641d59b53e00acefa563552c6c2d0f8288c4a32
Mathias Germandsen <141312755+MathiasGermandsen@users.noreply.github.com>
2024-01-30 14:24:47 +0100

    Wuhuuuu commit of  the day

---
+++ bilbassen2.0/Bilbassen2.0/Classes/CombustionCar.cs
@@ -0,0 +1,17 @@
+﻿namespace Classes
+{
+    public class CombustionCar
+    {
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public int HorsePower { get; set; }
+        public int CylinderCount { get; set; }
+        public int Price { get; set; }
+        public double EngineSize { get; set; }
+        public int KilometerDriven { get; set; }
+
+    }
+}
+
---
+++ bilbassen2.0/Bilbassen2.0/Classes/ElectricVehicles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class ElectricCar
+    {
+
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public int PowerHP { get; set; }
+        public int BatteryCapacityKWH { get; set; }
+        public int Price { get; set; }
+        public int KilometerDriven { get; set; }
+    }
+}
